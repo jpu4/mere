0d249e6bd393a6385a76d797481369be9d1079e0
James Ussery <james@ussery.me>
2020-01-06 22:26:10 +0530

    merge singletheme with master

--- config/config.php
+++ config/config.php
@@ -10,17 +10,24 @@ class config {
     public static bool $Debug = true;
     public static ?string $Env = 'dev';
     // SITE
-    public static ?string $Site_Name = 'twig1';
+    public static ?string $Site_Name = 'A Mere site';
+    public static ?string $Site_Author = 'James Ussery';
+    public static ?string $Site_Descr = 'A Mere site';
     public static ?string $Site_Baseurl = 'http://mere.jfa.lan';
-    public static ?string $Site_Theme = 'twentytwenty';
     public static ?string $DateTime_Format = 'Y-m-d H:i:s';         //date('Y-m-d H:i:s')
     public static ?string $DateTime_Io = 'Ymd-His';                 //date('Ymd-His')
     public static ?string $Site_Key = '0qY4reOLFH4nPGahkDbWuUdXL6O2/8k8sulz91SE8MU=';
+    // OWNER
+    public static ?string $Owner_Address1 = '123 Fiscal St';
+    public static ?string $Owner_Address2 = 'BLDG A';
+    public static ?string $Owner_CityStateZip = 'Dallas, TX 77777';
+    public static ?string $Owner_Phone = '123.123.1234';
+    public static ?string $Owner_Mobile = '123.123.1234';
+    public static ?string $Owner_Hours = 'Monday - Friday: 9:00 AM to 5:00 PM';
+
     // PATH
     public static ?string $DIR_Config = '/config';
-    public static ?string $DIR_Themes = '/themes';
-    public static ?string $DIR_Images = '/images';
-    public static ?string $DIR_Pages = '/pages';
+    public static ?string $DIR_Images = '/assets';
     public static ?string $DIR_Composer = '/vendor';
     // MAIL
     public static ?string $Mail_Type = 'smtp';
@@ -28,12 +35,37 @@ class config {
     public static ?string $Mail_Port = '';
     public static ?string $Mail_Username = '';
     public static ?string $Mail_Password = '';
-    public static ?string $Mail_Sendfrom = '';
+    public static ?string $Mail_SendTo = 'emailme@domain.com';
+    public static ?string $Mail_Sendfrom = 'contactform@domain.com';
     public static ?string $Mail_Supportemail = 'support@mach.us';
     // BACKUP
     public static ?string $Backup_Host = '';
     public static ?string $Backup_Username = '';
     public static ?string $Backup_Password = '';
+
+    public static function getOwnerInfo(){
+        $owner_info = array(
+            'address1' => self::$Owner_Address1,
+            'address2' => self::$Owner_Address2,
+            'citystatezip' => self::$Owner_CityStateZip,
+            'phone' => self::$Owner_Phone,
+            'mobile' => self::$Owner_Mobile,
+            'hours' => self::$Owner_Hours,
+            'email' => self::$Mail_SendTo
+        );
+        return $owner_info;
+    }
+    
+    public static function getSiteInfo(){
+        $site_info = array(
+            'name' => self::$Site_Name,
+            'author' => self::$Site_Author,
+            'description' => self::$Site_Descr,
+            'baseurl' => self::$Site_Baseurl,
+            'key' => self::$Site_Key
+        );
+        return $site_info;
+    }
 
     public static function getPath($which,$type='http'){
         $root='';
@@ -46,14 +78,8 @@ class config {
             case 'config':
                 return $root . self::$DIR_Config . '/';
             break;
-            case 'themes':
-                return $root . self::$DIR_Themes . '/';
-            break;
-            case 'images':
+            case 'assets':
                 return $root . self::$DIR_Images . '/';
-            break;
-            case 'pages':
-                return $root . self::$DIR_Pages . '/';
             break;
             case 'composer':
                 return $root . self::$DIR_Composer . '/';
@@ -62,11 +88,6 @@ class config {
         
     }
 
-    public static function getThemePath($type='abs'){
-
-        return self::getPath('themes',$type).self::$Site_Theme;
-
-    }
 
 }
 ?>
--- config/router.php
+++ config/router.php
@@ -5,13 +5,10 @@ use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
 $config = new config;
-$Path_Theme = $config->getThemePath();
-$Path_Pages = $config->getPath('pages','abs');
 $pagename='';
 
 $uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 $user_request = $uri[0];
-
 // send data to pages
 if ($user_request=='/'){ 
     $pagename='Home';
@@ -19,39 +16,42 @@ if ($user_request=='/'){
     $pagename = ucwords(str_replace('_',' ',str_replace('-',' ',trim($user_request,'/'))));  // converts "/about-us" to "About Us"
 };
 
-$twigvars = array(
-    'page' => array(
-        'name' => $pagename
-    ),
-    'config' => array(
-        'path_activetheme' => $config->getThemePath('http')
-    )
-);
-
 function renderTemplate($file, array $params = array()){
-    global $Path_Theme;
-	$loader = new \Twig\Loader\FilesystemLoader($Path_Theme);
+	$loader = new \Twig\Loader\FilesystemLoader('/');
 	//$twig = new Environment($loader, array('cache' => __DIR__ . '/cache'));
 	$twig = new Environment($loader, array('cache' => FALSE));
     echo $twig->render($file, $params);
 }
 
-function pageExists($path='',$url){
-    if ($url == '/'){ $url='/index'; }
-    $thisurl = $path . $url;
-    
+function pageExists($url){
+    // Find files that match url request from user
+    if ($url == '/'){ $url='/home'; }
+    $url = trim($url,'/');
+
     foreach (array('.php','.htm','.html','.twig') as $ext){
-        if (file_exists($thisurl.$ext)){
-            return $thisurl.$ext;
+        if (file_exists($url.$ext)){
+            return $url.$ext;
         } 
     }
 }
 
-if (is_null(pageExists($Path_Pages,$user_request))){ 
+if (is_null(pageExists($user_request))){ 
+    // Content page was not found, show 404
     header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-    require_once(pageExists($Path_Pages,'/404'));
+    renderTemplate(pageExists('/404'),[
+        'page' => array('name' => '404'), 
+        'site' => array('name' => $config::$Site_Name)
+        ]
+    );
     die;
+
 } else {
-    renderTemplate('base.twig', $twigvars);
+    // Load Content pages as twig templates and share data
+    renderTemplate(pageExists($user_request), [
+        'page' => array('name' => $pagename),
+        'site'=>$config->getSiteInfo(),
+        'owner'=>$config->getOwnerInfo()
+        ]
+    );
     
 }
