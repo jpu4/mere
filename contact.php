6c9568d9df0a6c0910ab0692c6ec0044ab47d433
James Ussery <james@ussery.me>
2020-01-08 14:59:20 +0530

    cleanup

--- contact.php
+++ contact.php
@@ -70,7 +70,7 @@ if($_SESSION["captcha"]==$_POST["captcha"])
 <header>
   <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="/"><?php echo $config::$Site_Name;?></a>
+      <a class="navbar-brand" href="/"><img src="assets/images/logo.jpg" alt="logo" style="width:20%;margin-right:5px;" /><?php echo $config::$Site_Name;?></a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
